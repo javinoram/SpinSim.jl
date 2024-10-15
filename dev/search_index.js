a89bb3e9b6efd2fb114df23a9a6d041ff72d2eda
Documenter.jl <documenter@juliadocs.github.io>
2024-10-15 17:46:19 +0000

    build based on 4ec9ce4

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Documentation-Test","page":"Home","title":"Documentation Test","text":"","category":"section"}]
+[{"location":"#SpinSim.jl","page":"Home","title":"SpinSim.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to this small package to construct the matrix representation of some spin model. This matrices are expected to be used for magnetocaloric studies or analize the spectral decomposition.","category":"page"},{"location":"","page":"Home","title":"Home","text":"This is the recopilation of all the work done to simulate and publish the result of the 3Ni SMM.","category":"page"}]
 }
